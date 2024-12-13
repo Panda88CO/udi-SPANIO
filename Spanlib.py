05ee2c367c145686202538184334d12823d7f4bc
Panda88CO <christian.olgaard@gmail.com>
2024-12-13 10:27:59 -0800

    Update Spanlib.py

--- Spanlib.py
+++ Spanlib.py
@@ -148,14 +148,14 @@ class SpanAccess(object):
         if  update_time != t_1hour and hour_ok:
             self.span_data['circuit_info'][breaker_id]['prod_1hour'] = (produced_energy-prod_1_hour)*3600/(update_time-t_1hour)
             self.span_data['circuit_info'][breaker_id]['cons_1hour'] = (consumed_energy-cons_1_hour)*3600/(update_time-t_1hour)
-            logging.debug(f'1 hour average: prod {produced_energy} - {prod_1_hour} - cons {consumed_energy} - {cons_1_hour} - time {update_time}-{t_1hour}')
+            logging.debug(f'{breaker_id} 1 hour average: prod {produced_energy} - {prod_1_hour} - cons {consumed_energy} - {cons_1_hour} - time {update_time}-{t_1hour}')
         else:
             self.span_data['circuit_info'][breaker_id]['prod_1hour'] = None
             self.span_data['circuit_info'][breaker_id]['cons_1hour'] = None
         if  update_time != t_24hour and day_ok:
             self.span_data['circuit_info'][breaker_id]['prod_24hour'] = (produced_energy-prod_24_hour)*24*3600/(update_time-t_24hour)
             self.span_data['circuit_info'][breaker_id]['cons_24hour'] = (consumed_energy-cons_24_hour)*24*3600/(update_time-t_24hour)
-            logging.debug(f'24 hour average: prod {produced_energy} - {prod_24_hour} - cons {consumed_energy} - {cons_24_hour} - time {update_time}-{t_24hour}')
+            logging.debug(f'{breaker_id}  24 hour average: prod {produced_energy} - {prod_24_hour} - cons {consumed_energy} - {cons_24_hour} - time {update_time}-{t_24hour}')
         else:
             self.span_data['circuit_info'][breaker_id]['prod_24hour'] = None
             self.span_data['circuit_info'][breaker_id]['cons_24hour'] = None
