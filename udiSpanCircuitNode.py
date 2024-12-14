e3234e3c68c54d91efb648dcf36688ad2dc3e5b0
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2024-12-13 23:01:09 -0800

    Update udiSpanCircuitNode.py

--- udiSpanCircuitNode.py
+++ udiSpanCircuitNode.py
@@ -70,12 +70,13 @@ class udiSpanCircuitNode(udi_interface.Node):
         self.my_setDriver('GV5', round(imp_wh, 2), 119 ) 
         self.my_setDriver('GV6', round(exp_wh, 2), 119 )   
         producedWh, consumerWh = self.span_panel.get1HourAverage(self.circuit)
-        if producedWh  and consumerWh:
+        
+        if type(producedWh) in (int, float) and type(consumerWh) in (int, float):            
             self.my_setDriver('GV7', round((producedWh- consumerWh),2), 119)
         else:
             self.my_setDriver('GV7', None, 25)
         producedWh, consumerWh = self.span_panel.get24HourAverage(self.circuit)   
-        if producedWh  and consumerWh:                             
+        if type(producedWh) in (int, float) and type(consumerWh) in (int, float):                                  
             self.my_setDriver('GV8', round((producedWh- consumerWh),2), 119) 
         else:
             self.my_setDriver('GV8', None, 25)           
